239abd6b9b346547d5761a17f48b79de437169a7
Armen S Tarbinian <139669232+ArmenTarb@users.noreply.github.com>
2024-12-06 16:41:45 -0800

    Add files via upload

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Chef.css';
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote_image" />
+          <p className="p__opensans">
+            Culture, family, tradition, tying all together through food.
+          </p>
+        </div>
+        <p className="p__opensans">
+          Ever since i was a child, eating at the family table with all my
+          relative was the highlight of every occassion. Our goal is to be able
+          to create that for anyone coming into our house ready to enjoy a meal.
+          With all the varieties available with the different seasons we
+          capitalize on all things naturally possible.
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
